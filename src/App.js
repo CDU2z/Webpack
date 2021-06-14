4e4fb0bfd063aaae724794c95048f1a024d46b3a
dlopez <dan.lopez@exodus.com.mx>
2021-06-14 14:30:48 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+const App = () => {
+  return (
+    <Fragment>
+      <h1>Template title</h1>
+      <h2>Template subtitle</h2>
+    </Fragment>
+  );
+};
+
+export default App;
